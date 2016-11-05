6dddcfc60b53de2d4e3dd29885f70d165b42d39a
anushabhimala <anushapriyabhimala@gmail.com>
2016-11-05 15:34:04 -0700

    created a model

---
+++ PrayerRequestsApp/PrayerRequestsApp/Models/PrayerRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrayerRequestsApp.Models
+{
+    public class PrayerRequest
+    {
+        public int prayerRequestId { get; set; }
+        public string prayerRequestText { get; set; }
+        public string submittedBy { get; set; }
+        public DateTime updatedDate { get; set; }
+        public string isAnswered { get; set; }
+
+    }
+}
